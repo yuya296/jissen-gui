176682cf7812927a0737f2ef721c910240a00916
yuya296 <yuya296@gmail.com>
2023-03-31 18:25:41 +0900

    軽微な修正

--- src/components/ui/CustomTable.tsx
+++ src/components/ui/CustomTable.tsx
@@ -7,32 +7,27 @@ type CustomTableProps = {
 }
 
 export const CustomTable = ({ data }: CustomTableProps) => {
-    const exissues = data.map(datum => (
-        <ListGroup.Item style={{ textAlign: 'left' }}>
-            <div className="font-monospace">
-                [{datum.code}]
-            </div>
-            <div className="fs-5">
-                {datum.name}
-            </div>
-        </ListGroup.Item>
-    ));
     const rule = {
         minimumFractionDigits: 1,
         maximumFractionDigits: 1,
     }
 
-
     return (
-            <Table striped hover>
+            <Table hover>
                 <tbody>
                     {data.map((item, index) => (
                         <tr key={index}>
                             <td className='text-start'>
-                                <div className="font-monospace">{item.code}</div>
-                                <div className="fs-5">{item.name}</div>
+                                <div className="font-monospace text-success">{item.code}</div>
+                                <div className="fs-5 fw-bold">{item.name}</div>
+                                <div className="text-secondary fw-bold">
+                                    利率: {item.rate} % | 
+                                    償還: {item.maturity} | 
+                                    クーポン: {item.couponTimes} 回
+                                </div>
                             </td>
-                            <td className="text-end" width={200}>
+                            <td className="text-end align-middle" width={200}>
+                                <div>数量: {item.quantity.toLocaleString('ja',rule)} 円</div>
                                 <div>簿価: {item.bookValue.toLocaleString('ja',rule)} 円</div>
                                 <div>時価: {item.marketValue?.toLocaleString('ja',rule)??'#N/A'} 円</div>
                                 <div>評価損益: {item.profitAndLoss?.toLocaleString('ja',rule)??'#N/A'} 円</div>
